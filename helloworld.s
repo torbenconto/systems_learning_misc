7368dc6804dc98ea3bc011ab10a8c8421020f7f2
torbenconto <torben@2x1.dev>
2023-10-10 11:23:46 -0400

    comments

--- helloworld.s
+++ helloworld.s
@@ -21,19 +21,19 @@
 ;
 
 section .data:
-    message db "Hello, World!", 0xa
-    len equ $ - message
+    message db "Hello, World!", 0xa ; Set message, 0xa is a null terminator like \0 in C
+    len equ $ - message             ; Must define length in asm
 
 
 section .text:
     global _start
 
 _start:
-    mov edx,len     ; Message Length
-    mov ecx,message ; Message to Write
-    mov ebx,1       ; Stdout
-    mov eax,4       ; System_write call number
-    int 0x80        ; Call Kernel
+    mov edx, len     ; Message Length
+    mov ecx, message ; Message to Write
+    mov ebx, 1       ; Stdout
+    mov eax, 4       ; System_write call number
+    int 0x80         ; Call Kernel
 
-    mov eax,1       ; Sys exit
+    mov eax, 1       ; Sys exit
     int 0x80         ; Call Kernel
