5069a753e9a53a1df51e1a438ccd9aea3752ffe4
torbenconto <torben@2x1.dev>
2023-10-11 09:48:51 -0400

    add table of sys calls

--- systemcalls.s
+++ systemcalls.s
@@ -20,6 +20,15 @@
 ; THE SOFTWARE.
 ;
 
+; Table of system calls
+; %eax 	Name 	%ebx 	%ecx 	%edx 	%esx 	%edi
+; 1 	sys_exit 	int 	- 	- 	- 	-
+; 2 	sys_fork 	struct pt_regs 	- 	- 	- 	-
+; 3 	sys_read 	unsigned int 	char * 	size_t 	- 	-
+; 4 	sys_write 	unsigned int 	const char * 	size_t 	- 	-
+; 5 	sys_open 	const char * 	int 	int 	- 	-
+; 6 	sys_close 	unsigned int 	- 	- 	- 	-
+;
 section .data
     inputMsg db "Enter a number: "
     lenInputMsg equ $ - inputMsg
